caf72f36827a6bc5223f3c7680aaa58b152fef2d
francoleon08 <francoleon284@gmail.com>
2025-10-18 11:13:18 -0300

    feat: enhance policy registration with toast notifications and improved error handling

--- actions/policies.ts
+++ actions/policies.ts
@@ -3,125 +3,131 @@
 import { createClient } from '@supabase/supabase-js'
 import { Database, Tables } from '@/types/database'
 import { cookies } from 'next/headers'
-import {
-  InsuranceType,
-  LifePolicyData,
-  HomePolicyData,
-  VehiclePolicyData,
-  QuoteData,
-  PolicyCategory,
-} from '@/lib/policy-plan'
-
+import { LifePolicyData, HomePolicyData, VehiclePolicyData, QuoteData, PolicyCategory } from '@/lib/policy-plan'
 
 const supabase = createClient<Database>(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 )
 
-export async function createContractedPolicy(quoteData: QuoteData): Promise<{ success: boolean; error?: string; id?: string }>{
-  
+export async function createContractedPolicy(
+  quoteData: QuoteData
+): Promise<{ success: boolean; error?: string; id?: string }> {
   const userID = await getUserIdFromCookie()
-  if(!userID.success) return {success: false, error: userID.error}
-  console.log('USER ID: ', userID.id)
-  
+  if (!userID.success)
+    return {
+      success: false,
+      error: userID.error,
+    }
+
   const planID = await getPlanId(quoteData.selectedPlan as PolicyCategory)
-  if(!planID.success) return {success: false, error: planID.error}
-  console.log('PLAN ID: ', planID.id)
+  if (!planID.success) return { success: false, error: planID.error }
 
   const generalPolicyID = await createGeneralPolicy(userID.id as string, planID.id as string)
-  if(!generalPolicyID.success) return {success: false, error: generalPolicyID.error}
-  console.log('NEW POLICY ID: ', generalPolicyID.id)
+  if (!generalPolicyID.success) return { success: false, error: generalPolicyID.error }
 
-
-  switch(quoteData.insuranceType){
-    case "life": 
+  switch (quoteData.insuranceType) {
+    case 'life':
       await createLifePolicy(generalPolicyID.id as string, quoteData.policyData as LifePolicyData)
-      break;
-    case "home": 
+      break
+    case 'home':
       await createHomePolicy(generalPolicyID.id as string, quoteData.policyData as HomePolicyData)
-      break;
-    case "vehicle": 
-      await createVehiclePolicy(generalPolicyID.id as string, quoteData.policyData as VehiclePolicyData);
+      break
+    case 'vehicle':
+      await createVehiclePolicy(generalPolicyID.id as string, quoteData.policyData as VehiclePolicyData)
   }
 
-  return {success: true, id: generalPolicyID.id}
+  return { success: true, id: generalPolicyID.id }
 }
 
-async function getUserIdFromCookie(): Promise<{success: boolean; error?: string; id?: string}> {
-  const cookieStore = cookies();
-  const sessionToken = cookieStore.get('auth-session')?.value;
-  console.log(sessionToken)
-  if (!sessionToken) throw new Error("Invalid session token");
-
-  const {data, error} = await supabase
-    .from('sessions')
-    .select('user_id')
-    .eq('id', sessionToken)
-    .single()
-  
-  if (error) return {success: false, error: error.message}
-  if(!data.user_id) throw new Error("null user id")
-  return {success: true, id: data.user_id};
+interface SessionTokenId {
+  user: {
+    id: string
+  }
 }
 
-async function getPlanId(type: PolicyCategory): Promise<{success: boolean; error?: string; id?: string}> {
-  if(type === null) throw new Error("null insurance type")
-  const {data, error} = await supabase
-    .from('plans')
-    .select('id')
-    .eq('category', type) //.charAt(0).toUpperCase() + type.slice(1)
-    .single()
+async function getUserIdFromCookie(): Promise<{ success: boolean; error?: string; id?: string }> {
+  const cookieStore = cookies()
+  const sessionToken = cookieStore.get('auth-session')?.value
+  if (!sessionToken) throw new Error('Invalid session token')
 
-  if (error) return {success: false, error: error.message}
-  return {success: true, id: data.id};
+  const sessionTokenObj: SessionTokenId = JSON.parse(sessionToken)
+  return { success: true, id: sessionTokenObj.user.id }
 }
 
-async function createGeneralPolicy(userID: string, planID: string): Promise<{success: boolean; error?: string; id?: string}>{
+async function getPlanId(type: PolicyCategory): Promise<{ success: boolean; error?: string; id?: string }> {
+  if (type === null) throw new Error('null insurance type')
+  const { data, error } = await supabase.from('plans').select('id').eq('category', type).single()
+
+  if (error) return { success: false, error: error.message }
+  return { success: true, id: data.id }
+}
+
+async function createGeneralPolicy(
+  userID: string,
+  planID: string
+): Promise<{ success: boolean; error?: string; id?: string }> {
   const { data, error } = await supabase
-  .from('contracted_policy')
-  .insert([
-    { user_id: userID, policy_type_id: planID, policy_number: Math.random()},
-  ])
-  .select('id')
-  .single()
+    .from('contracted_policy')
+    .insert([{ user_id: userID, policy_type_id: planID, policy_number: Math.random() }])
+    .select('id')
+    .single()
 
-  if (error) return {success: false, error: error.message}
-  return {success: true, id: data.id};
+  if (error) return { success: false, error: error.message }
+  return { success: true, id: data.id }
 }
 
 async function createLifePolicy(policyID: string, lifePolicyData: LifePolicyData) {
+  const jsonCertData = {
+    certData: lifePolicyData.certData || 'Sin certificado',
+  }
+
+  const isDataPresented = lifePolicyData.certData !== null && lifePolicyData.certData !== undefined
+
   const { data, error } = await supabase
     .from('life_policy')
-    .insert([
-      { id: policyID, 
-        cert_data: JSON.parse(lifePolicyData.certData), 
-        cert_presented: lifePolicyData.certPresented },
-    ])
-    if (error) throw new Error(error.message)
+    .insert([{ id: policyID, cert_data: JSON.stringify(jsonCertData), cert_presented: isDataPresented }])
+
+  if (error) throw new Error(error.message)
 }
 
 async function createHomePolicy(policyID: string, homePolicyData: HomePolicyData) {
-  const { data, error } = await supabase
-    .from('home_policy')
-    .insert([
-      { id: policyID,
-        construction_type: homePolicyData.constructionType, 
-        building_age: homePolicyData.buildingAge,
-        city: homePolicyData.city,
-        neighborhood: homePolicyData.neighborhood },
-    ])
-    if (error) throw new Error(error.message)
+  const insertData = {
+    id: policyID,
+    construction_type: homePolicyData.constructionType.toLowerCase(),
+    building_age: homePolicyData.buildingAge,
+    city: homePolicyData.city,
+    neighborhood: homePolicyData.neighborhood,
+  }
+
+  const { data, error } = await supabase.from('home_policy').insert([insertData])
+  if (error) throw new Error(error.message)
 }
 
 async function createVehiclePolicy(policyID: string, vehiclePolicyData: VehiclePolicyData) {
-  const { data, error } = await supabase
-    .from('vehicle_policy')
-    .insert([
-      { id: policyID,
-        vehicle_year: vehiclePolicyData.vehicleYear, 
-        vehicle_model: vehiclePolicyData.vehicleModel,
-        vehicle_theft_risk: vehiclePolicyData.vehicleTheftRisk,
-        driver_violations: JSON.parse(vehiclePolicyData.driverViolations) },
-    ])
-    if (error) throw new Error(error.message)
+  let numberRisk;
+  switch (vehiclePolicyData.vehicleTheftRisk) {
+    case 'low':
+      numberRisk = 1;
+      break;
+    case 'medium':
+      numberRisk = 2;
+      break;
+    case 'high':
+      numberRisk = 3;
+      break;
+    default:
+      numberRisk = 0;
+  }
+
+  const insertData = {
+    id: policyID,
+    vehicle_year: vehiclePolicyData.vehicleYear,
+    vehicle_model: vehiclePolicyData.vehicleModel,
+    vehicle_theft_risk: numberRisk,
+    driver_violations: JSON.parse(vehiclePolicyData.driverViolations),
+  }
+
+  const { data, error } = await supabase.from('vehicle_policy').insert([insertData])
+  if (error) throw new Error(error.message)
 }
--- app/api/policy/register/route.ts
+++ app/api/policy/register/route.ts
@@ -1,33 +1,27 @@
-import { NextRequest, NextResponse } from "next/server";
-import { createContractedPolicy } from "@/actions/policies";
-import { QuoteData } from "@/lib/policy-plan";
+import { NextRequest, NextResponse } from 'next/server'
+import { createContractedPolicy } from '@/actions/policies'
+import { QuoteData } from '@/lib/policy-plan'
 
 export async function POST(request: NextRequest) {
   try {
-    const body = await request.json();
-    const { insuranceType, clientType, multiplier, policyData, selectedPlan, basePrices } = body;
+    const body = await request.json()
+    const { insuranceType, clientType, multiplier, policyData, selectedPlan, basePrices } = body
     const quoteData: QuoteData = body
 
     const res = await createContractedPolicy(quoteData)
-    if(!res.success){
-      return NextResponse.json(
-        { error: res.error },
-        { status: 400 }
-      );
+
+    if (!res.success) {
+      return NextResponse.json({ error: res.error }, { status: 400 })
     }
 
     return NextResponse.json(
-        {
-          message: "Registro de póliza exitoso",
-        },
-        { status: 200 }
-      );
-
+      {
+        message: 'Registro de póliza exitoso',
+      },
+      { status: 200 }
+    )
   } catch (error) {
-    console.error("[POST /api/policy/register]", error);
-    return NextResponse.json(
-      { error: "Error interno del servidor" },
-      { status: 500 }
-    );
+    console.error('[POST /api/policy/register]', error)
+    return NextResponse.json({ error: 'Error interno del servidor' }, { status: 500 })
   }
 }
