a6bfd380f67c50bcac3a0f467bcccec3ddcc515f
francoleon08 <francoleon284@gmail.com>
2025-11-01 10:34:38 -0300

    feat: enhance EmployeeDashboard with navigation buttons and add policy pages

---
+++ app/dashboard/employee/policies/create/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function RegisterPolicyPage() {
+    return (
+        <div>
+            <h1>Registrar Póliza</h1>
+        </div>
+    );
+}
---
+++ app/dashboard/employee/policies/existing/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function ExistingPoliciesPage() {
+    return (
+        <div>
+            <h1>Pólizas Existentes</h1>
+        </div>
+    );
+}
